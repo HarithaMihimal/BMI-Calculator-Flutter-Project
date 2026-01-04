ab58b7456b5730b77206c4e1d201c012b3e2ab27
HarithaMihimal <harithamihimal4@gmail.com>
2026-01-04 09:08:21 +0530

    first commit

---
+++ lib/bmi_calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BmiCalculatorPage extends StatefulWidget {
+  const BmiCalculatorPage({super.key});
+
+  @override
+  State<BmiCalculatorPage> createState() => _BmiCalculatorPageState();
+}
+
+class _BmiCalculatorPageState extends State<BmiCalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("hello world");
+  }
+}
